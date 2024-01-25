c866b7576f1aa5a12e6989b97e12505f7d1e6599
JuanMaDev <86654681+0MFCAT@users.noreply.github.com>
2024-01-25 00:30:00 -0500

    Fixed the answer to the /start command

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ async def handle_webhook(update: TelegramUpdate, token: str = Depends(auth_teleg
         await bot.send_message(chat_id=chat_id, text="Welcome to Cyclic Starter Python Telegram Bot!")
     else:
         await bot.send_message(chat_id=chat_id, reply_to_message_id=update.message["message_id"], text="Yo!")"""
-        await update.message.reply_text(f"Hello,\nThis bot increases the hours played of games on Steam\\.\nType *_/help_* to learn how to use it\\.\n\n[_Source Code_](https://github.com/tingirifistik/Steam-HourBooster)\n[_Twitter_](https://twitter.com/_tingirifistik)", parse_mode='MarkdownV2')
+        await update.send_message(chat_id=chat_id, reply_to_message_id=update.message["message_id"], text=f"Hola,\nEste bot te ayudará a aumentar tus horas de juego en Steam\\.\nEscribe *_/help_* para ver los comandos disponibles\\.\n\n[_Gracias a:_](https://github.com/tingirifistik/Steam-HourBooster)\n[_Mi Youtube_](https://www.youtube.com/channel/UCElCoULDa68Yzqi1slcWvKA?sub_confirmation=1)", parse_mode='MarkdownV2')
         
 
     return {"ok": True}
