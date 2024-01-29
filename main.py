3af78a45ebd98b0588ce14c1941d9ed5b2619f59
JuanMaDev <86654681+0MFCAT@users.noreply.github.com>
2024-01-29 15:20:31 -0500

    now the bot does not reply to messages

--- main.py
+++ main.py
@@ -39,25 +39,25 @@ async def handle_webhook(update: TelegramUpdate, token: str = Depends(auth_teleg
     # print("Received message:", update.message)
 
     if text == "/start":
-        await bot.send_message(chat_id=chat_id, reply_to_message_id=update.message["message_id"], text=f"👋 Hola,\nEste bot te ayudará a aumentar tus horas de juego en Steam\\.\nEscribe *_/help_* para ver los comandos disponibles\\.\n\n[_Mi Youtube_](https://www.youtube.com/channel/UCElCoULDa68Yzqi1slcWvKA?sub_confirmation=1)", parse_mode='MarkdownV2')
+        await bot.send_message(chat_id=chat_id, text=f"👋 Hola,\nEste bot te ayudará a aumentar tus horas de juego en Steam\\.\nEscribe *_/help_* para ver los comandos disponibles\\.\n\n[_Mi Youtube_](https://www.youtube.com/channel/UCElCoULDa68Yzqi1slcWvKA?sub_confirmation=1)", parse_mode='MarkdownV2')
     elif text == "/help":
-        await bot.send_message(chat_id=chat_id, reply_to_message_id=update.message["message_id"], text=f"❓ Usando este bot es muy simple aumentar tus horas de juego en Steam\\.\n\nComandos disponibles:\n *_/account_* \- configura tus credenciales de Steam, va seguido de tu nombre de usuario de steam y contraseña, separados por espacios\\.\n *_/games_* \- va seguido del ID de los juegos de Steam que quieres jugar separados por espacios. Máximo 30 juegos\\.\n *_/farm_* \- comienza a farmear horas, va seguido de la cantidad de horas que quieres farmear\\.\n\n[_Mi Youtube_](https://www.youtube.com/channel/UCElCoULDa68Yzqi1slcWvKA?sub_confirmation=1)", parse_mode='MarkdownV2')
+        await bot.send_message(chat_id=chat_id, text=f"❓ Usando este bot es muy simple aumentar tus horas de juego en Steam\\.\n\nComandos disponibles:\n *_/account_* \- configura tus credenciales de Steam, va seguido de tu nombre de usuario de steam y contraseña, separados por espacios\\.\n *_/games_* \- va seguido del ID de los juegos de Steam que quieres jugar separados por espacios. Máximo 30 juegos\\.\n *_/farm_* \- comienza a farmear horas, va seguido de la cantidad de horas que quieres farmear\\.\n\n[_Mi Youtube_](https://www.youtube.com/channel/UCElCoULDa68Yzqi1slcWvKA?sub_confirmation=1)", parse_mode='MarkdownV2')
     elif text.startswith("/account"):
         try:
             user_input = update.message["text"].split()
             steam_user = user_input[1]
             steam_password = user_input[2]
         except IndexError:
-            await bot.send_message(chat_id=chat_id, reply_to_message_id=update.message["message_id"], text=f"🚫 Mal uso del comando *_/account_*, el formato correcto sería: \\ \n\n*_/account username password_*", parse_mode='MarkdownV2')
+            await bot.send_message(chat_id=chat_id, text=f"🚫 Mal uso del comando *_/account_*, el formato correcto sería: \\ \n\n*_/account username password_*", parse_mode='MarkdownV2')
             return
-        await bot.send_message(chat_id=chat_id, reply_to_message_id=update.message["message_id"], text=f"*_Usuario de Steam_*:  {steam_user}\\\n*_Password de Steam_*: {steam_password}\\\n\n💾 Nueva configuración de la cuenta guardada", parse_mode='MarkdownV2')
+        await bot.send_message(chat_id=chat_id, text=f"*_Usuario de Steam_*:  {steam_user}\\\n*_Password de Steam_*: {steam_password}\\\n\n💾 Nueva configuración de la cuenta guardada", parse_mode='MarkdownV2')
     elif text.startswith("/games"):
             steam_games = update.message["text"].split()[1:]
             if steam_games:
                 steam_games = list(map(lambda x: int(x), steam_games))
-                await bot.send_message(chat_id=chat_id, reply_to_message_id=update.message["message_id"], text=f"🎮 Lista de juegos actualizada a: \\ \n\n{steam_games}", parse_mode='MarkdownV2')                
+                await bot.send_message(chat_id=chat_id, text=f"🎮 Lista de juegos actualizada a: \\ \n\n{steam_games}", parse_mode='MarkdownV2')                
             else:
-                await bot.send_message(chat_id=chat_id, reply_to_message_id=update.message["message_id"], text=f"🚫 Mal uso del comando *_/games_*, el formato correcto sería: \\ \n\n*_/games IDgame1 IDgame2 IDgame3 \\.\\.\\. IDgame30_*\\ \n\nEl máximo de juegos a la vez es 30", parse_mode='MarkdownV2')
+                await bot.send_message(chat_id=chat_id, text=f"🚫 Mal uso del comando *_/games_*, el formato correcto sería: \\ \n\n*_/games IDgame1 IDgame2 IDgame3 \\.\\.\\. IDgame30_*\\ \n\nEl máximo de juegos a la vez es 30", parse_mode='MarkdownV2')
                 return
     elif text.startswith("/farm"):
         try:
@@ -65,15 +65,15 @@ async def handle_webhook(update: TelegramUpdate, token: str = Depends(auth_teleg
             farming_time = int(user_input[1])
             steam_guard_code = user_input[2]
         except IndexError:
-            await bot.send_message(chat_id=chat_id, reply_to_message_id=update.message["message_id"], text=f"🚫 Mal uso del comando *_/farm_*, el formato correcto sería: \\ \n\n*_/farm time SteamGuardCode_*\\ \n\nDebes tener Steam Guard activo y poner el tiempo en horas exactas", parse_mode='MarkdownV2')
+            await bot.send_message(chat_id=chat_id, text=f"🚫 Mal uso del comando *_/farm_*, el formato correcto sería: \\ \n\n*_/farm time SteamGuardCode_*\\ \n\nDebes tener Steam Guard activo y poner el tiempo en horas exactas", parse_mode='MarkdownV2')
             return
         
         #account_login = client.login(username=steam_user, password=steam_password, two_factor_code=steam_guard_code)
         #client.games_played(steam_games)
         #client.run_forever()
-        await bot.send_message(chat_id=chat_id, reply_to_message_id=update.message["message_id"], text=f"Farmeando con éxito por {farming_time} horas en los juegos {steam_games}", parse_mode='MarkdownV2')
+        await bot.send_message(chat_id=chat_id, text=f"Farmeando con éxito por {farming_time} horas en los juegos {steam_games}", parse_mode='MarkdownV2')
     else:
-        await bot.send_message(chat_id=chat_id, reply_to_message_id=update.message["message_id"], text=f"🚫 Ese no es un comando válido, para más información de como usar el bot usa el comando *_/help_* o mira el tutorial en mi [_canal de Youtube_](https://www.youtube.com/channel/UCElCoULDa68Yzqi1slcWvKA?sub_confirmation=1)", parse_mode='MarkdownV2')
+        await bot.send_message(chat_id=chat_id, text=f"🚫 Ese no es un comando válido, para más información de como usar el bot usa el comando *_/help_* o mira el tutorial en mi [_canal de Youtube_](https://www.youtube.com/channel/UCElCoULDa68Yzqi1slcWvKA?sub_confirmation=1)", parse_mode='MarkdownV2')
         
     
     return {"ok": True}
